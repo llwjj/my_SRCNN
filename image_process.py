c22010d9c70dc19e061e53a41b35a87f2d31eb2a
lwj <2271918836@qq.com>
2019-05-06 16:41:53 +0800

    Create image_process.py

---
+++ image_process.py
@@ -0,0 +1,20 @@
+import cv2
+
+
+def LR_image(img,de_num=4):
+    shape = img.shape
+
+    img = cv2.resize(img,(shape[1]//de_num,shape[0]//de_num))
+    img = cv2.resize(img,(shape[1],shape[0]))
+    return img
+
+def imread(path,type=None):
+    return cv2.imread(path)
+def imshow(img):
+    cv2.imshow('',img)
+    cv2.waitKey(-1)
+
+def resize(img,scale):
+    h,w,_ = img.shape
+    shape = (int(w*scale),int(h*scale))
+    return cv2.resize(img,shape)
