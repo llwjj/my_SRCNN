63916635b7792712b9ccaba6c33fb01d13c484de
lwj <2271918836@qq.com>
2019-05-10 11:07:50 +0800

    Update image_process.py

--- image_process.py
+++ image_process.py
@@ -1,11 +1,13 @@
 import cv2
+import keras.backend as K
 
 
-def LR_image(img,de_num=4):
+def LR_image(img,de_num=4,inplace=False):
     shape = img.shape
 
     img = cv2.resize(img,(shape[1]//de_num,shape[0]//de_num))
-    img = cv2.resize(img,(shape[1],shape[0]))
+    if not inplace:
+        img = cv2.resize(img,(shape[1],shape[0]))
     return img
 
 def imread(path,type=None):
